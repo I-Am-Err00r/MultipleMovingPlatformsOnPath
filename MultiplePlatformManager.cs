c18432a2df199ec4f84b980cac11a75c7af3cd08
I_Am_Err00r <40243263+I-Am-Err00r@users.noreply.github.com>
2020-12-06 17:24:31 -0600

    Add files via upload

---
+++ MultiplePlatformManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplePlatformManager : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject platformToSpawn;
+    [SerializeField]
+    protected enum Direction { Clockwise, CounterClockwise }
+    [SerializeField]
+    protected Direction direction;
+    [SerializeField]
+    protected bool spawnPlatformsAtPath;
+    [SerializeField]
+    protected float speed;
+    [Tooltip("This is the paths the platforms will follow; if spawnPlatformsAtPath not checked, the platforms will all spawn at each path point")]
+    public List<Vector2> numberOfPaths = new List<Vector2>();
+    [Tooltip("If you want to setup custom locations on the path for the platforms, you set the initial position of each platform here")]
+    public List<Vector2> platformSpawns = new List<Vector2>();
+    [Tooltip("If you want to setup custom locations on the path for the platforms, you set the initial index on the path of each platform here; if the platform is in between two paths, the greater of the two is where it would move to for clockwise")]
+    public List<int> platformSpawnNextIndex = new List<int>();
+    protected List<GameObject> platforms = new List<GameObject>();
+
+    protected virtual void Start()
+    {
+        if (spawnPlatformsAtPath)
+        {
+            for (int i = 0; i < numberOfPaths.Count; i++)
+            {
+                platforms.Add(Instantiate(platformToSpawn, numberOfPaths[i], Quaternion.identity));
+                platforms[i].transform.position = new Vector3(platforms[i].transform.position.x, platforms[i].transform.position.y, 0);
+                platforms[i].GetComponent<MoveablePlatform>().index = i;
+                CheckPosition(platforms[i]);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < platformSpawns.Count; i++)
+            {
+                platforms.Add(Instantiate(platformToSpawn, platformSpawns[i], Quaternion.identity));
+                platforms[i].transform.position = new Vector3(platforms[i].transform.position.x, platforms[i].transform.position.y, 0);
+                if (direction == Direction.Clockwise)
+                {
+                    platforms[i].GetComponent<MoveablePlatform>().index = platformSpawnNextIndex[i];
+                }
+                else
+                {
+                    int next = platformSpawnNextIndex[i] - 1;
+                    if(next < 0)
+                    {
+                        next = numberOfPaths.Count - 1;
+                    }
+                    platforms[i].GetComponent<MoveablePlatform>().index = next;
+
+                }
+                CheckPosition(platforms[i]);
+            }
+        }
+
+    }
+
+    protected virtual void LateUpdate()
+    {
+        foreach (GameObject platform in platforms)
+        {
+            MovePlatform(platform);
+        }
+    }
+
+    protected virtual void MovePlatform(GameObject platform)
+    {
+        Vector3 position = new Vector3(numberOfPaths[platform.GetComponent<MoveablePlatform>().index].x, numberOfPaths[platform.GetComponent<MoveablePlatform>().index].y, 0);
+        platform.transform.position = Vector2.MoveTowards(platform.transform.position, position, Time.deltaTime * speed);
+        if (platform.transform.position == position)
+        {
+            CheckPosition(platform);
+        }
+    }
+
+    protected virtual void CheckPosition(GameObject platform)
+    {
+        if (direction == Direction.Clockwise)
+        {
+            platform.GetComponent<MoveablePlatform>().index++;
+            if (platform.GetComponent<MoveablePlatform>().index == numberOfPaths.Count)
+            {
+                platform.GetComponent<MoveablePlatform>().index = 0;
+            }
+        }
+        else
+        {
+            platform.GetComponent<MoveablePlatform>().index--;
+            if (platform.GetComponent<MoveablePlatform>().index == -1)
+            {
+                platform.GetComponent<MoveablePlatform>().index = numberOfPaths.Count - 1;
+            }
+        }
+    }
+}
---
+++ PathEditor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MultiplePlatformManager), true)]
+public class PathEditor : Editor
+{
+    protected virtual void OnSceneGUI()
+    {
+        int next = new int();
+        Handles.color = Color.green;
+        MultiplePlatformManager path = target as MultiplePlatformManager;
+        for (int i = 0; i < path.numberOfPaths.Count; i++)
+        {
+            Vector2 position = path.numberOfPaths[i];
+            next = i + 1;
+            if (next == path.numberOfPaths.Count)
+            {
+                next = 0;
+            }
+            position = Handles.FreeMoveHandle(position, Quaternion.identity, .5f, new Vector2(.5f, .5f), Handles.CircleHandleCap);
+            Handles.DrawDottedLine(path.numberOfPaths[i], path.numberOfPaths[next], 5);
+            Handles.Label(path.numberOfPaths[i], "Path" + i.ToString());
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(target, "MoveablePlatform");
+                path.numberOfPaths[i] = position;
+            }
+        }
+        Handles.color = Color.red;
+        MultiplePlatformManager platform = target as MultiplePlatformManager;
+        for (int i = 0; i < platform.platformSpawns.Count; i++)
+        {
+            Vector2 position = platform.platformSpawns[i];
+            next = i + 1;
+            if (next == platform.platformSpawns.Count)
+            {
+                next = 0;
+            }
+            position = Handles.FreeMoveHandle(position, Quaternion.identity, .5f, new Vector2(.5f, .5f), Handles.RectangleHandleCap);
+            Handles.DrawDottedLine(platform.platformSpawns[i], platform.platformSpawns[next], 5);
+            Handles.Label(platform.platformSpawns[i], "Platform" + i.ToString());
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(target, "MoveablePlatform");
+                platform.platformSpawns[i] = position;
+            }
+        }
+    }
+}
