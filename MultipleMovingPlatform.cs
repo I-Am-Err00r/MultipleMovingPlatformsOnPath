5a11447185bb6e19351809ad876f2231f2ede4ca
I_Am_Err00r <40243263+I-Am-Err00r@users.noreply.github.com>
2021-11-18 08:19:00 -0600

    Add files via upload

---
+++ MultipleMovingPlatform.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class MultipleMovingPlatform : MonoBehaviour
+{
+    [HideInInspector]
+    public int index;
+}
